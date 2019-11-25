2b7d3f42f8d73b45e345623d28cda8f27245041a
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2019-11-24 20:11:33 -0500

    Integration may be done

--- train_frcnn.py
+++ train_frcnn.py
@@ -233,11 +233,11 @@ for epoch_num in range(num_epochs):
 			    print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
 			    if mean_overlapping_bboxes == 0:
 			      print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
-			X, Y, img_data = next(data_gen_train)
+			X_defect, X_template, Y, img_data = next(data_gen_train)
 
-			loss_rpn = model_rpn.train_on_batch(X, Y)
+			loss_rpn = model_rpn.train_on_batch([X_defect, X_template], Y)
 
-			P_rpn = model_rpn.predict_on_batch(X)
+			P_rpn = model_rpn.predict_on_batch([X_defect, X_template])
 			R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.4, max_boxes=300)
 			# note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
 			X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
@@ -282,7 +282,7 @@ for epoch_num in range(num_epochs):
 			    else:
                                 sel_samples = random.choice(pos_samples)
 
-			loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+			loss_class = model_classifier.train_on_batch([X_defect, X_template, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
 
 			losses[iter_num, 0] = loss_rpn[1]
 			losses[iter_num, 1] = loss_rpn[2]
